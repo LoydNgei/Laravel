9d470d0be406a4562a27a778c3f2970a5b349815
Loyd Ngei <loydngei@gmail.com>
2024-01-14 11:06:26 +0300

    changes on Web.php

--- lsapp/routes/web.php
+++ lsapp/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/hello', function () {
-    return view('Hello World');
-});
+Route::get('/', [PagesController::class, 'index']);
+
+Route::get('/about', [PagesController::class, 'about']);
+
+Route::get('/services', [PagesController::class, 'services']);
+
