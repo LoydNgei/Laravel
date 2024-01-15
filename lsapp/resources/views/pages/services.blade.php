0739d03a745a4ab787e846079b569771e55f9f63
Loyd Ngei <loydngei@gmail.com>
2024-01-15 23:15:26 +0300

    Blade templating

--- lsapp/resources/views/pages/services.blade.php
+++ lsapp/resources/views/pages/services.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'LSAPP')}}</title>
 
-    </head>
-    <body class="antialiased">
-        <h1>Services Page</h1>
-        <p>This is the services page</p>
-    </body>
-</html>
+@section('content')
+<h1>Services Page</h1>
+<p>This is the services page</p>
+@endsection
