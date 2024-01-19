b4149b9b305b71440df84ec5b4248f319251a6e2
Loyd Ngei <loydngei@gmail.com>
2024-01-19 22:23:57 +0300

    Lists changes

--- lsapp/app/Models/Listing.php
+++ lsapp/app/Models/Listing.php
@@ -5,6 +5,16 @@
 
     class Listing {
         public static function all() {
-            
+
+        }
+
+        public static function find($id) {
+            $listings = self::all();
+
+            foreach($listings as listing){
+                if($listing['id'] == $id) {
+                    return $listing;
+                }
+            }
         }
     }
