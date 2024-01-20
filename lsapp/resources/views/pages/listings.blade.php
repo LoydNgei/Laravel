a457dd602a16b3a04845bf03fb24fc18624e0cff
Loyd Ngei <loydngei@gmail.com>
2024-01-20 23:47:26 +0300

    listings blade

--- lsapp/resources/views/pages/listings.blade.php
+++ lsapp/resources/views/pages/listings.blade.php
@@ -1,12 +1,14 @@
-<h1>{{}}</h1>
+<h1>{{ $heading }}</h1>
 
 @unless(count($listings) == 0)
+
+
 @foreach($listings as $listing)
 <h2>
-    {{$listing['title']}}
+    <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
 </h2>
 <p>
-    {{$listing('description')}}
+    {{$listing['description']}}
 </p>
 @endforeach
 
