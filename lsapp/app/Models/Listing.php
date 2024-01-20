32b8c8c130682fdb0b470febe9db120595d2f124
Loyd Ngei <loydngei@gmail.com>
2024-01-20 23:17:31 +0300

    Listing changes

--- lsapp/app/Models/Listing.php
+++ lsapp/app/Models/Listing.php
@@ -1,20 +1,11 @@
 <?php
 
-    namespace App\Models;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    class Listing {
-        public static function all() {
-
-        }
-
-        public static function find($id) {
-            $listings = self::all();
-
-            foreach($listings as listing){
-                if($listing['id'] == $id) {
-                    return $listing;
-                }
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
+}
