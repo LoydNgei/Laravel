94ae06b774dcb7e8597d3bb3be0c672f7342c7e7
Loyd Ngei <loydngei@gmail.com>
2024-01-28 22:34:45 +0300

    Web.php

--- lsapp/routes/web.php
+++ lsapp/routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
-
 use App\Http\Controllers\ListingController;
 
 
@@ -35,7 +34,6 @@ use App\Http\Controllers\ListingController;
 // All Listings
 
 Route::get('/', [ListingController::class, 'index']);
-
 
 
 // Single Listing -> ROUTE BINDING TECHNIQUE
