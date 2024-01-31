25d47c214d62a1852a4cee8355cd883d51ef0947
Loyd Ngei <loydngei@gmail.com>
2024-01-31 08:50:46 +0300

    User controller

--- lsapp/app/Http/Controllers/UserController.php
+++ lsapp/app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -11,16 +13,27 @@ class UserController extends Controller
         return view('users.register');
     }
 
+    // Create New User
     public function store(Request $request) {
-        $formfields = $request->validate([
+        $formFields = $request->validate([
             'name' => ['required', 'min:3'],
             'email' => ['required', 'email', Rule::unique('users', 'email')],
-            'password' => ['required, confirmed, min:6']
+            'password' => 'required|confirmed|min:6'
         ]);
 
         // Hash Password
-        
-        $formfields['password'] = bcrypt($formFields['password']);
+        $formFields['password'] = bcrypt($formFields['password']);
+
+
+        // Create User
+        $user = User::create($formFields);
+
+        // Login User
+        auth()->login($user);
+
+        return redirect('/')->with('message', 'User created and logged in');
+
+
     }
 
 }
