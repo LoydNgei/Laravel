9cca2ca71ee9b92249b2bf987c6f7d57dc015906
Loyd Ngei <loydngei@gmail.com>
2024-01-31 08:51:03 +0300

    Register blade

--- lsapp/resources/views/users/register.blade.php
+++ lsapp/resources/views/users/register.blade.php
@@ -7,8 +7,9 @@
                 <p class="mb-4">Create an account to post gigs</p>
             </header>
 
-            <form mrthod="POST" action="/users">
+            <form method="POST" action="/users">
                 @csrf
+                {{-- @method('PUT') --}}
                 <div class="mb-6">
                     <label for="name" class="inline-block text-lg mb-2">
                         Name
@@ -17,6 +18,7 @@
                         type="text"
                         class="border border-gray-200 rounded p-2 w-full"
                         name="name"
+                        value="{{old('name')}}"
                     />
 
                     @error('name')
@@ -32,6 +34,7 @@
                         type="email"
                         class="border border-gray-200 rounded p-2 w-full"
                         name="email"
+                        value="{{old('email')}}"
                     />
                     @error('email')
                         <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
@@ -49,6 +52,7 @@
                         type="password"
                         class="border border-gray-200 rounded p-2 w-full"
                         name="password"
+                        value="{{old('password')}}"
                     />
                     @error('password')
                         <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
