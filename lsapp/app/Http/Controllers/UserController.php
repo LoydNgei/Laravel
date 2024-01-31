c9d1c637f94f29ae0a5ac38f4b6b5d005b0e89b4
Loyd Ngei <loydngei@gmail.com>
2024-01-31 11:57:02 +0300

    Adding more user functions(Logout)

--- lsapp/app/Http/Controllers/UserController.php
+++ lsapp/app/Http/Controllers/UserController.php
@@ -8,12 +8,15 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-    //Create Form
+    // Create Form
+
     public function create() {
         return view('users.register');
+
     }
 
     // Create New User
+
     public function store(Request $request) {
         $formFields = $request->validate([
             'name' => ['required', 'min:3'],
@@ -22,18 +25,55 @@ class UserController extends Controller
         ]);
 
         // Hash Password
+
         $formFields['password'] = bcrypt($formFields['password']);
 
 
         // Create User
+
         $user = User::create($formFields);
 
         // Login User
+
         auth()->login($user);
 
         return redirect('/')->with('message', 'User created and logged in');
 
 
+    }
+
+    // Logout method
+
+    public function logout(Request $request) {
+        auth()->logout();   // Terminate the User's authentication session
+
+        $request->session()->invalidate(); // Clears all session data for security
+
+        $request->session()->regenerateToken(); // Creates a new session token to prevent seession hijacking
+
+        return redirect('/')->with('message', 'You have been logged out');    
+    }
+
+    // Login method
+
+    public function login() {
+        return view('users.login');
+    }
+
+    // Authenticate User
+    public function authenticate(Request $request) {
+        $formFields = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($formFields)) {
+            $request->session()->regenerate();
+
+            return redirect('/')->with('message', 'You are now logged in');
+        }
+
+        return back()->withErrors(['email' => 'Invalid Credentials'])->onlyInput('email');
     }
 
 }
